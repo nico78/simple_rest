8a0c34b10d28a51ffccbc1c02224e7a75fcd5dc6
nico78 <nico78@users.noreply.github.com>
2016-04-24 15:15:09 +0100

    Update SimpleRestApplication.java

--- src/main/java/com/codurance/SimpleRestApplication.java
+++ src/main/java/com/codurance/SimpleRestApplication.java
@@ -2,12 +2,16 @@ package com.codurance;
 
 import org.eclipse.jetty.http.HttpStatus;
 import spark.Route;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.Enumeration;
 
 import static spark.Spark.get;
 
 public class SimpleRestApplication {
     public static void main(String[] args) {
-        get("/hello", respondWith(new HelloWorld().sayHelloWorld()));
+        get("/hello", respondWith("Hello I'm running on : \n" + getIp()));
         get("/healthcheck", respondWith("OK"));
     }
 
@@ -17,5 +21,21 @@ public class SimpleRestApplication {
             response.status(HttpStatus.OK_200);
             return responseBody;
         };
+    }
+    
+       private static String getIp() throws SocketException {
+        Enumeration e = NetworkInterface.getNetworkInterfaces();
+        StringBuilder sb = new StringBuilder();
+        while(e.hasMoreElements())
+        {
+            NetworkInterface n = (NetworkInterface) e.nextElement();
+            Enumeration ee = n.getInetAddresses();
+            while (ee.hasMoreElements())
+            {
+                InetAddress i = (InetAddress) ee.nextElement();
+                sb.append(i.getHostAddress());
+            }
+        }
+        return sb.toString();
     }
 }
